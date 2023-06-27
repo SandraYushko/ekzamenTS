51f741ae559aa359091b198b988368344d3502b5
SandraYushko <shyria6@gmail.com>
2023-06-27 20:05:07 +0300

    Ponedelnik. Ekzamen #3 zadanie 9

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,27 @@
-import React, {MouseEvent} from 'react';
+import React, {useState, MouseEvent} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 function Button() {
+    const [tagName, setTagName] = useState<string>()
     const onClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
-        console.log((typeof e) === 'object')
+        setTagName(e.currentTarget.tagName)
     }
-    return <button onClick={onClickHandler} >Click</button>
+    return (
+        <>
+            <p>{tagName}</p>
+            <button onClick={onClickHandler} >
+                <span>Click</span>
+            </button>
+        </>
+    )
 }
-
 
 ReactDOM.render(
     <Button/>, document.getElementById('root')
 );
 
-// Какой тип данных представляет аргумент функции-обработчика?
-// Что надо написать вместо ххх, что бы в консоль вывело true?
-
+// Что надо написать вместо ххх, что бы на странице появился текст BUTTON?
 
 
 
